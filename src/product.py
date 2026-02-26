709883457d86dee03594f18650f38f09cea2c11d
Vadim Semenov <vadimsemenov53@gmail.com>
2026-02-26 19:21:46 +0300

    add class Product & Category

---
+++ src/product.py
@@ -0,0 +1,15 @@
+
+
+class Product:
+    """Класс для представления Java-разработчиков."""
+    name : str
+    description : str
+    price : float
+    quantity : int
+
+    def __init__(self, name, description, price, quantity):
+        """"""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
