1e5730950acbdfa16d1772b0bfd808a903a714fb
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-09 10:13:08 +0300

    add fuxture for testing smartphone and lawngrasss

--- tests/conftest.py
+++ tests/conftest.py
@@ -69,7 +69,23 @@ def smartphone_obj_class_1():
 
 
 @pytest.fixture()
+def smartphone_obj_class_2():
+    Product.clear_products()
+    return Smartphone("Iphone 15", "512GB, Gray space",
+                      210000.0, 8, 98.2,
+                      "15", 512, "Gray space")
+
+
+
+@pytest.fixture()
 def lawngrass_obj_class_1():
     Product.clear_products()
     return LawnGrass("Газонная трава", "Элитная трава для газона",
                      500.0, 20, "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture()
+def lawngrass_obj_class_2():
+    Product.clear_products()
+    return LawnGrass("Газонная трава 2", "Выносливая трава",
+                     450.0, 15, "США", "5 дней", "Темно-зеленый")
