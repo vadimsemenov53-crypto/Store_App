744a238badc39b7679899beeb4b7c4b3ab97c849
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-09 09:25:18 +0300

    add class Smartphone __init__

---
+++ src/smartphone_product.py
@@ -0,0 +1,17 @@
+from src.product import Product
+
+class Smartphone(Product):
+    """Подкласс (Product), для представления смартфонов."""
+    efficiency: float
+    model: str
+    memory: (int | float)
+    color: str
+
+    def __init__(self, name: str, description: str, price: float, quantity: int,
+                 efficiency, model, memory, color):
+        """Метод, который инициализирует экземпляры класса."""
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
