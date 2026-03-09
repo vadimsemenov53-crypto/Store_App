fc3af8860b3a6164ed63cddd5eecbb4bd6763d05
Vadim Semenov <vadimsemenov53@gmail.com>
2026-03-09 13:27:14 +0500

    add testing add_product

--- tests/test_category.py
+++ tests/test_category.py
@@ -41,7 +41,7 @@ def test_category_add(data_category, data_cat_add_product):
 
 
 def test_category_add_error(data_category):
-    with pytest.raises(TypeError, match="Можно добавлять только объекты Product"):
+    with pytest.raises(TypeError, match="Можно добавлять только объекты Product или его наследников"):
         data_category.add_product({"name": "Iphone"})
 
 
@@ -51,3 +51,38 @@ def test_category_str(data_category, data_cat_add_product):
     data_category.add_product(data_cat_add_product)
 
     assert str(data_category) == "Смартфоны, количество продуктов: 28 шт."
+
+
+def test_category_add_another(data_category, smartphone_obj_class_1, lawngrass_obj_class_1):
+    assert (
+            data_category.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+                                      "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+                                      "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+    )
+    assert Category.category_count == 1
+    assert Category.product_count == 3
+
+    data_category.add_product(smartphone_obj_class_1)
+
+    assert (
+            data_category.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+                                      "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+                                      "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
+                                      "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    )
+
+    assert Category.category_count == 1
+    assert Category.product_count == 4
+
+    data_category.add_product(lawngrass_obj_class_1)
+
+    assert (
+            data_category.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+                                      "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+                                      "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
+                                      "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+                                      "Газонная трава, 500.0 руб. Остаток: 20 шт."
+    )
+
+    assert Category.category_count == 1
+    assert Category.product_count == 5
